1a71a83f0f6de260d075ee204d0395ac49b3d502
AlexPlom <alexiosplomaritis@gmail.com>
2018-09-21 17:07:44 +0300

    Adds PandoraConfigurationProvider to pandora package

---
+++ src/Elders.Pandora/PandoraConfigurationProvider.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Linq;
+
+namespace Elders.Pandora
+{
+    public class PandoraConfigurationProvider : ConfigurationProvider
+    {
+        private readonly Pandora pandora;
+
+        public PandoraConfigurationProvider(Pandora pandora)
+        {
+            this.pandora = pandora;
+        }
+
+        public override void Load()
+        {
+            Data = pandora.GetAll().ToDictionary(key => key.Key.SettingKey, value => value.Value);
+        }
+    }
+}
